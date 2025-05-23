127f349f482df325621cfb5a342e0551a61e7912
Vleontueso78 <vleontueso78@gmail.com>
2025-05-23 19:30:34 -0300

    guardando practica

--- mapa.py
+++ mapa.py
@@ -1,15 +1,32 @@
+mesas = 2
+sillas = 3
+
+#Wif mesas in sillas ({sillas}):
+      #for mesas in range(3):
+        #print(mesas)
+#elif sillas in mesas(3 + 2):
+     #for sillas in range(2):
+        # print (sillas)        
 
 
+#Actividad práctica: Suma de números introducidos por el usuario
+#Ahora, te propongo una actividad para practicar el uso del bucle for:
 
+#Enunciado:
 
-mapa = [
-      [" ","A", "B", "C", "D", "E"],
-      ["1","~", "~", "~", "~", "~"],
-      ["2","~", "~", "~", "~", "~"],
-      ["3","~", "~", "~", "~", "~"],
-      ["4","~", "~", "~", "~", "~"],
-      ["5","~", "~", "~", "~", "~"],
-  ]
+#Escribe un programa que:
 
-for fila in mapa:
-    print(fila)
+#Pregunte cuántos números se van a introducir.
+
+#Pida esos números (que pueden ser decimales) y calcule su suma.
+
+#Pista: Puedes utilizar la función input() para obtener datos del usuario y la función float() para convertir esos datos en números decimales.
+
+usuario = input("cuantos numeros vas a introducir(1/5) : ")
+for i in range(1,5):
+ numeros = usuario 
+while numeros in int<= 5: 
+ print (numeros)
+ numeros += 1
+ print (usuario) 
+
