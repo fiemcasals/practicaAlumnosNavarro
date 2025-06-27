2beac40ef5bc2fa5f7b09f5f2c161f8891503d3d
Vleontueso78 <vleontueso78@gmail.com>
2025-06-26 21:38:37 -0300

    Sigoc con el traductor

---
+++ Ejercicios/Tateti/juego.py
@@ -0,0 +1,13 @@
+# import funciones
+
+# Creo el tablero vació...
+# El primer for me crea una lista con tres elementos " " (Vector). 
+# Y el segundo for me crea 3 listas iguales que la anterior adentro de una lista (Matrix).
+tablero = [[" " for _ in range(3)] for _ in range(3)] 
+
+# print(tablero) 
+
+# Muestro el tablero...
+#funciones.mostrar_tablero(tablero)
+
+# funciones.pedir_movimiento(tablero, jugador)
